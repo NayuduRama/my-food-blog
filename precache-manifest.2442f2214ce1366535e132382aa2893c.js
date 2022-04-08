46356d806971b4b7dd79ebec04bd3caa29c601ac
NayuduRama <sadamramanayudu@gmail.com>
2022-04-08 16:54:07 +1000

    Updates

---
+++ precache-manifest.2442f2214ce1366535e132382aa2893c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c7a6888ca15769a3d59",
+    "url": "/my-food-blog/static/css/main.f2d7b8c1.chunk.css"
+  },
+  {
+    "revision": "1c7a6888ca15769a3d59",
+    "url": "/my-food-blog/static/js/main.1c7a6888.chunk.js"
+  },
+  {
+    "revision": "aa1ecd07d8b85afa671e",
+    "url": "/my-food-blog/static/js/1.aa1ecd07.chunk.js"
+  },
+  {
+    "revision": "e42b120e36acb8c41379",
+    "url": "/my-food-blog/static/css/2.a018a985.chunk.css"
+  },
+  {
+    "revision": "e42b120e36acb8c41379",
+    "url": "/my-food-blog/static/js/2.e42b120e.chunk.js"
+  },
+  {
+    "revision": "46ab614c3fd574c934c4",
+    "url": "/my-food-blog/static/js/runtime~main.46ab614c.js"
+  },
+  {
+    "revision": "486d87f1f8036077bcbab2310252c76f",
+    "url": "/my-food-blog/static/media/logo.486d87f1.svg"
+  },
+  {
+    "revision": "42df5494d499960f12f8fa410fc02b67",
+    "url": "/my-food-blog/static/media/search.42df5494.svg"
+  },
+  {
+    "revision": "133f35110181656391f20404dd172ebc",
+    "url": "/my-food-blog/static/media/burger.133f3511.svg"
+  },
+  {
+    "revision": "e97d7eaceea2887b4a8a867968e223f0",
+    "url": "/my-food-blog/index.html"
+  }
+];
